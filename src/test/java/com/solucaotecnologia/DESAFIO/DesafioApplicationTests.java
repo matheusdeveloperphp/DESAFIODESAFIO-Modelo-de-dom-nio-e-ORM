0dcca383984f807ecc46d225766f594fb3c34444
matheusdeveloperphp <matheus.lima@blueservice.com.br>
2025-02-20 20:21:10 -0300

    initial project

---
+++ src/test/java/com/solucaotecnologia/DESAFIO/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solucaotecnologia.DESAFIO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
